5960f0c41fcac45044f56aa6aedfa8c177a2e632
Ziji Shi <shi.ziji.sm@gmail.com>
2018-09-02 03:39:35 +0800

    workshop 1 - update

--- 1-Compilation/ex3/pi.cpp
+++ 1-Compilation/ex3/pi.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 // try different num_steps and compare the results
 // note the range and precision of data type
@@ -8,6 +9,8 @@ long double step;
 
 int main ()
 { 
+    clock_t begin = clock();
+
     int i; 
     long double x, pi, sum = 0.0;
     step = 1.0/(double) num_steps;
@@ -19,6 +22,10 @@ int main ()
     pi = step * sum;
     std::cout.precision(20);
     std::cout<<"Pi = "<< pi <<std::endl;
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Elapsed CPU time is %f\n",time_spent);
 
     return 0;
 }
